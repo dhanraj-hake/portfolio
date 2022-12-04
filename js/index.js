94f880feddde535c97b6e1a234a0113a0618b2fd
Harsh <harshhake1212@gmail.com>
2022-12-04 15:52:37 +0530

    Portfolio

--- js/index.js
+++ js/index.js
@@ -1,10 +1,18 @@
 
-const menuIcon = document.getElementById("menu-icon");
+const menuIcon = document.getElementById("menu-icon-box");
 
 
 menuIcon.addEventListener("click", ()=>{
     document.getElementById("menu").classList.toggle("show-menu");
-    console.log("Cliked")
+    if(document.getElementById("menu-icon").classList.contains("fa-bars")){
+        document.getElementById("menu-icon").classList.remove("fa-bars");
+        document.getElementById("menu-icon").classList.add("fa-xmark");
+        console.log("class change");
+    }
+    else{
+        document.getElementById("menu-icon").classList.remove("fa-xmark");
+        document.getElementById("menu-icon").classList.add("fa-bars");
+    }
 })
 
 let links = document.getElementsByClassName("link");
@@ -13,7 +21,7 @@ links = Array.from(links)
 
 links.forEach((e)=>{
     e.addEventListener("click", ()=>{
-        document.getElementById("menu").classList.remove("show-menu")
+        document.getElementById("menu").classList.remove("show-menu");
     })
 });
 
